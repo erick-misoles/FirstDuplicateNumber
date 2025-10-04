d9e419c94dbaf6f9f697d211baa312c5965501c6
Shuichi-code <erick.vanjoeff.misoles@gmail.com>
2025-10-04 09:38:08 +0800

    initial commit

---
+++ FirstDuplicateNumber/FirstDuplicateNumber.Tests/DuplicateFinderTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstDuplicateNumber.Helpers;
+using System;
+
+namespace FirstDuplicateNumber.Tests
+{
+    [TestClass]
+    public class DuplicateFinderTests
+    {
+        [TestMethod]
+        public void FindFirstDuplicate_WithDuplicates_ReturnsFirstDuplicate()
+        {
+            int[] numbers = { 2, 1, 3, 5, 3, 2 };
+            var result = DuplicateFinder.FindFirstDuplicate(numbers);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void FindFirstDuplicate_NoDuplicates_ReturnsNull()
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            var result = DuplicateFinder.FindFirstDuplicate(numbers);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FindFirstDuplicate_EmptyArray_ReturnsNull()
+        {
+            int[] numbers = { };
+            var result = DuplicateFinder.FindFirstDuplicate(numbers);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindFirstDuplicate_NullArray_ThrowsArgumentNullException()
+        {
+            int[] numbers = null;
+            DuplicateFinder.FindFirstDuplicate(numbers);
+        }
+    }
+}
---
+++ FirstDuplicateNumber/FirstDuplicateNumber/Helpers/FirstDuplicateNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace FirstDuplicateNumber.Helpers
+{
+    public static class DuplicateFinder
+    {
+        public static int? FindFirstDuplicate(int[] numbers)
+        {
+            if (numbers == null) throw new ArgumentNullException(nameof(numbers));
+
+            var seen = new HashSet<int>();
+
+            foreach (var num in numbers)
+            {
+                if (seen.Contains(num))
+                {
+                    return num;
+                }
+                seen.Add(num);
+            }
+
+            return null;
+        }
+    }
+}
